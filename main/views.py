45a70ca9d284d0650bc6e73375a7063fe49edb6b
Sulthan Adrin <sulthanadrin0806@gmail.com>
2024-09-23 14:00:50 +0700

    menyelesaikan Tugas 4 kecuali README

--- main/views.py
+++ main/views.py
@@ -3,24 +3,72 @@ from main.forms import ProductForm
 from main.models import Product
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
 # Create your views here.
+@login_required(login_url='/login')
 def show_main(request):
-    product_entry = Product.objects.all()
+    product_entry = Product.objects.filter(user=request.user)
 
     context = {
         'app_name' : '3Thrift',
-        'name': 'Sulthan Adrin Pasha Siregar',
+        'name': request.user.username,
         'class': 'PBP C',
-        'product_entry': product_entry
+        'product_entry': product_entry,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
+
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
 
 def create_new_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        product_entry = form.save(commit=False)
+        product_entry.user = request.user
+        product_entry.save()
         return redirect('main:show_main')
 
     context = {'form': form}
